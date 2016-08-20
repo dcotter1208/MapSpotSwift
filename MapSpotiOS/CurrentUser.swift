ca38777e4e013af388cb7fd17bf2b7dd499aa099
Cotter <Cotter@DonovanDLabsMacBookPro.local>
2016-08-20 17:35:27 -0400

    added comments in CurrentUser Singleton class

--- MapSpotiOS/CurrentUser.swift
+++ MapSpotiOS/CurrentUser.swift
@@ -6,6 +6,21 @@
 //  Copyright © 2016 Cotter. All rights reserved.
 //
 
+
+/*
+ 
+ 
+ 1)  Create a class variable as a computed type property. The class variable, like class methods in Objective-C, is something you can call without having to instantiate the class CurrentUser.
+ 
+ 2)  Nested within the class variable is a struct called Singleton.
+ 
+ 3)  Singleton wraps a static constant variable named instance. Declaring a property as static means this property only exists once. Also note that static properties in Swift are implicitly lazy, which means that Instance is not created until it’s needed. Also note that since this is a constant property, once this instance is created, it’s not going to create it a second time. This is the essence of the Singleton design pattern. The initializer is never called again once it has been instantiated.
+ 
+ 4)  Returns the computed type property.
+
+ 
+*/
+
 import Foundation
 
 class CurrentUser: NSObject {
